afd9604379b34e240947ae15389c5976e509edcf
Parthtinna <parthtinna79@gmail.com>
2025-09-17 10:38:11 +0000

    Learn about memory heap and Stack

--- 01_BASICS/DataTypes-summary.js
+++ 01_BASICS/DataTypes-summary.js
@@ -12,7 +12,7 @@
  
  console.log(id === anotherId);
 
- const bigNumber = 32345593939393939n
+//  const bigNumber = 32345593939393939n
  
 
  // Reference (Non - Primitive)
@@ -29,7 +29,38 @@ let myobj ={
 
 const myFunction = function(){
     console.log("Hello World");
-}
+};
 console.log(typeof myFunction);
 
 console.log( typeof anotherId);
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// all premitive values saved in stack ( copy of that value is returned)
+
+// all Non premitive values are not saved on stack rather referece of heap memory is saved on stack 
+
+let myName = "Parth Tinna"
+
+let anotherName = myName
+anotherName = "Broskiii"
+console.log(myName);
+console.log(anotherName);
+
+let user1 = {
+    email:"user@google.com",
+    upi:"user@byl"
+}
+
+let user2 = user1
+
+console.log(user1);
+
+user2.email = "parthtinna@google.com"
+
+console.log(user1.email);
+console.log(user2.email);
